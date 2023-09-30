1e51d2af065f9cebfaeecc88d8f447bcf32d9356
runtimeterrorist <67608248+runtimeterrorist@users.noreply.github.com>
2023-09-30 22:15:04 +0200

    code improvements

--- pyPEOF.py
+++ pyPEOF.py
@@ -1,55 +1,61 @@
-from pefile import PE ,PEFormatError
-import argparse
 import os
+import argparse
+from pefile import PE, PEFormatError
 
-parser = argparse.ArgumentParser()
-parser.add_argument("-f", "--file", required=True, type=open,help="PE file to read")
-args = parser.parse_args()
-file = args.file
 
-def readEof(filepath):
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-f", "--file", required=True,
+                        type=open, help="PE file to read")
+    args = parser.parse_args()
+    file = args.file
+    print("[+] Reading PE file...")
+
     try:
-        imageSize = 0
-        pefile = PE(filepath)
+        image_size = 0
+        pefile = PE(file.name)
 
-        if(hex(pefile.DOS_HEADER.e_magic) != "0x5a4d"): # check if pe file is valid,e_magic must be 0x5a4d
-            print("PE FILE IS INVALID")
+        # check if pe file is valid,e_magic must be 0x5a4d
+        if (hex(pefile.DOS_HEADER.e_magic) != "0x5a4d"):
+            print("[+] PE file is invalid!")
         else:
-            print("PE FILE IS VALID") 
+            print("[+] PE file is valid!")
 
-        if hex(pefile.FILE_HEADER.Machine) == "0x14c":  # checking architecture
-            print("Image architecture is 32bit.")
-            imageSize += (pefile.OPTIONAL_HEADER.SizeOfHeaders + pefile.OPTIONAL_HEADER.DATA_DIRECTORY[4].Size ) # adding the IMAGE_DIRERCTORY_ENTRY_SECURITY if target application is signed otherwise the full image size wont match.
-        else:
-            print("Image architecture is 64bit.")
-            imageSize += (pefile.OPTIONAL_HEADER.SizeOfHeaders + pefile.OPTIONAL_HEADER.DATA_DIRECTORY[4].Size ) # adding the IMAGE_DIRERCTORY_ENTRY_SECURITY if target application is signed otherwise the full image size wont match.
+        architecture = 32 if hex(pefile.FILE_HEADER.Machine) == "0x14c" else 64
+        print("[+] Image architecture is %sbit." % architecture)
+        # adding the IMAGE_DIRERCTORY_ENTRY_SECURITY if target application is signed otherwise the full image size wont match.
+        image_size += (pefile.OPTIONAL_HEADER.SizeOfHeaders +
+                       pefile.OPTIONAL_HEADER.DATA_DIRECTORY[4].Size)
 
-        for section in pefile.sections: # enumerate each section and add to current mesured image size.
-            imageSize += section.SizeOfRawData
-            
-        AFileSize = os.path.getsize(filepath)
-        EOFSize = (AFileSize - imageSize)
+        # enumerate each section and add to current mesured image size.
+        for section in pefile.sections:
+            image_size += section.SizeOfRawData
 
-        if EOFSize>0 :         #checking if eof data is present in target file.
-            print("%s bytes of EOF data detected." %EOFSize)
+        AFileSize = os.path.getsize(file.name)
+        eof_size = (AFileSize - image_size)
+
+        if eof_size > 0:  # checking if eof data is present in target file.
+            print("[+] %s bytes of EOF data detected." % eof_size)
             # read eof data #
-            with open(filepath,"rb") as fp:
-                fp.seek(-EOFSize, 2)
-                eofdata = fp.read(EOFSize)
-                prompt = input("Do you want to print the EOF data? (y/n): ")
-                if prompt == "y" or prompt == "yes":
-                    print("Printing EOF data: \n%s" %eofdata)
-                
-                prompt = input("Do you want to dump the EOF data? (y/n): ")
-                if prompt == "y" or prompt == "yes":
-                    with open("%s.dump"%filepath,"wb") as dump:
-                        dump.write(eofdata)
-                        print("EOF data successfully dumped!")
-        else:       
-            print("No EOF data detected.")
-    except  PEFormatError:
+            file.seek(-eof_size, 2)
+            eof_data = file.read(eof_size)
+            prompt = input(
+                "Do you want to print the EOF data? (y/n): ")
+            if prompt == "y" or prompt == "yes":
+                print("[+] Printing EOF data: \n%s" % eof_data)
+
+            prompt = input("Do you want to dump the EOF data? (y/n): ")
+            if prompt == "y" or prompt == "yes":
+                with open("%s.dump" % file.name, "wb") as dump:
+                    dump.write(eof_data)
+                    print("[+] EOF data successfully dumped!")
+        else:
+            print("[+] No EOF data detected.")
+    except PEFormatError:
         print("Are you sure you're trying to read a PE file?")
     except OSError:
-        print("%s does not exist or is inaccessible." %filepath) 
+        print("[+] %s does not exist or is inaccessible." % file.name)
 
-readEof(file.name)
+
+if __name__ == "__main__":
+    main()
